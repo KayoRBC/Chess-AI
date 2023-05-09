7502236c6b36cd164d4de23ea0f006f20f380f43
KayoRBC <kayo10br@gmail.com>
2023-05-09 20:59:18 -0300

    feat: Add BishopMovementRules implementation and test

--- src/test/java/com/Kayo/controller/BoardControllerTest.java
+++ src/test/java/com/Kayo/controller/BoardControllerTest.java
@@ -12,6 +12,10 @@ class BoardControllerTest {
     @Test
     void movePawnFront() {
         BoardController controller = new BoardController();
+        boolean front0 = controller.move(1, 0, 1, 0);
+        Assertions.assertTrue(controller.getTypeOf(1, 0) == PieceType.PAWN);
+        Assertions.assertFalse(front0);
+
 
         // movendo 1 para frente
         boolean front1 = controller.move(1, 0, 2, 0);
@@ -59,15 +63,47 @@ class BoardControllerTest {
         Assertions.assertTrue(controller.getTypeOf(2, 1) == PieceType.NULL);
         Assertions.assertFalse(diagonal1);
 
-        // movimento certo diagonal
+        // preparando ambiente de teste
         controller.move(1, 6, 3, 6);
         controller.move(3, 6, 4, 6);
         controller.move(4, 6, 5, 6);
 
+        // movimento certo diagonal
         boolean diagonal2 = controller.move(5,6, 6, 7);
         Assertions.assertTrue(controller.getTypeOf(5, 6) == PieceType.NULL);
         Assertions.assertTrue(controller.getTypeOf(6, 7) == PieceType.PAWN);
         Assertions.assertTrue(diagonal2);
 
+    }
+
+    @Test
+    void moveBishop(){
+        BoardController controller = new BoardController();
+
+        // movimento errado
+        boolean diagonal0 = controller.move(7, 2, 7, 2);
+        Assertions.assertTrue(controller.getTypeOf(7, 2) == PieceType.BISHOP);
+        Assertions.assertFalse(diagonal0);
+
+        // movimento errado
+        boolean diagonal1 = controller.move(7, 2, 2, 7);
+        Assertions.assertTrue(controller.getTypeOf(7, 2) == PieceType.BISHOP);
+        Assertions.assertTrue(controller.getTypeOf(2, 7) == PieceType.NULL);
+        Assertions.assertFalse(diagonal1);
+
+        // preparando ambiente
+        controller.move(6, 3, 5, 3);
+
+        // movimento certo
+        boolean diagonal2 = controller.move(7, 2, 2, 7);
+        Assertions.assertTrue(controller.getTypeOf(7, 2) == PieceType.NULL);
+        Assertions.assertTrue(controller.getTypeOf(2, 7) == PieceType.BISHOP);
+        Assertions.assertTrue(diagonal2);
+
+        // movimento certo
+        boolean diagonal3 = controller.move(2, 7, 1, 6);
+        Assertions.assertTrue(controller.getTypeOf(2, 7) == PieceType.NULL);
+        Assertions.assertTrue(controller.getTypeOf(1, 6) == PieceType.BISHOP);
+        Assertions.assertTrue(diagonal3);
     }
 }
