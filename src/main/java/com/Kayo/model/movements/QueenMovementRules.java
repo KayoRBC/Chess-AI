5d73bd08e66e07d3fae0d2af6efb640a43682c04
KayoRBC <kayo10br@gmail.com>
2023-05-10 19:31:18 -0300

    feat: Add QueenMovementRules implementation

--- src/main/java/com/Kayo/model/movements/QueenMovementRules.java
+++ src/main/java/com/Kayo/model/movements/QueenMovementRules.java
@@ -5,6 +5,12 @@ import com.Kayo.model.Board;
 public class QueenMovementRules extends MovementRules{
     @Override
     public boolean isValidMove(Board board, int fromLine, int fromColumn, int toLine, int toColumn) {
-        return false;
+        MovementRules diagonalMovement = new BishopMovementRules();
+        MovementRules frontMovement = new RookMovementRules();
+
+        boolean canMove1 = diagonalMovement.isValidMove(board, fromLine, fromColumn, toLine, toColumn);
+        boolean canMove2 = frontMovement.isValidMove(board, fromLine, fromColumn, toLine, toColumn);
+
+        return canMove1 || canMove2;
     }
 }
