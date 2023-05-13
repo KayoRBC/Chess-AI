d1c02ce6e65d9d026b1e2a4c86f8f6b25ca881c6
KayoRBC <kayo10br@gmail.com>
2023-05-13 04:47:09 -0300

    refactor: Delete package movements

--- src/main/java/com/Kayo/model/chass/movements/MovementRules.java
+++
@@ -1,8 +0,0 @@
-package com.Kayo.model.chass.movements;
-
-import com.Kayo.model.chass.Board;
-
-public abstract class MovementRules {
-
-    public abstract boolean isValidMove(Board board, int fromLine, int fromColumn, int toLine, int toColumn);
-}
